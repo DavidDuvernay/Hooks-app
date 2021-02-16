4846230bfb1006012446bbddb70f8943a5e61913
David Duvernay <david.anderson0612@gmail.com>
2021-02-15 21:42:56 -0500

    Modulate TodoApp en TodoList. splitted in two more components to make it easier to comprehend components

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,7 +1,8 @@
 import React, { useReducer, useEffect } from 'react';
 import { todoReducer } from './todoReducer';
-import { useForm } from '../hooks/useForm';
 import './styles.css'
+import { TodoList } from './TodoList';
+import { TodoAdd } from './TodoAdd';
 
 const init = () => {
 
@@ -12,43 +13,9 @@ export const TodoApp = () => {
 
     const [ todos, dispatch ] = useReducer(todoReducer, [], init);
    
-    const [{ description }, handleInputChange, reset] = useForm({
-        description: '',
-
-    });
-
     useEffect( () => {
         localStorage.setItem('todos', JSON.stringify(todos));
     }, [todos]);
-
-    // const handleDelete = (todoId) => {
-    //     console.log(todoId)
-    // };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        if(description.trim().length <= 1 ) {
-
-            return;
-        };
-
-        const newTodo = {
-            id: new Date().getTime(),
-            desc: description,
-            done: false,
-
-        };
-
-        const action = {
-            type: 'add',
-            payload: newTodo
-        };
-
-        dispatch(action);
-        reset();
-
-    };
 
     const handleDelete = (todoId) => {
         
@@ -65,8 +32,16 @@ export const TodoApp = () => {
         dispatch({
             type: 'toggle',
             payload: todoId,
-        });
-    }
+        })
+    };
+
+    const handleAddTodo = (newTodo) => {
+        
+        dispatch({
+            type: 'add',
+            payload: newTodo
+        })
+    };
 
     return(
         <div>
@@ -75,53 +50,17 @@ export const TodoApp = () => {
 
             <div className= 'row'>
                 <div className= 'col-7'>
-                    <ul className= 'list-group list-group-flush'>
-                        {
-                            todos.map(  (todo, i) => (
-                                <li
-                                    key= {todo.id}
-                                    className= 'list-group-item'
-                                > <p 
-                                    onClick= { () => handleToggle( todo.id )}
-                                    className= {`${ todo.done  && 'complete'}`}
-                                > 
-                                {i + 1}. { todo.desc }
-                                </p>
-                                    <button
-                                        className= 'btn btn-danger'
-                                        onClick = { () => handleDelete( todo.id ) }
-                                    >
-                                        Borrar
-                                    </button>
-                                </li>
-                            ))
-                        }
-                    </ul>
+                    
+                    <TodoList 
+                        todos= { todos }
+                        handleDelete = { handleDelete }
+                        handleToggle = { handleToggle }
+                    />
                 </div>
 
                 <div className= 'col-5'>
-                    <h4>Agregar todo</h4>
-                    <hr />
-
-                    <form onSubmit= { handleSubmit }>
-                        <input
-                            type= 'text'
-                            name= 'description'
-                            className= 'form-control'
-                            placeholder= 'Aprender...'
-                            autoComplete= 'off'
-                            value= { description }
-                            onChange= { handleInputChange }
-                        ></input>
-
-                        <button
-                            type= 'submit'
-                            className= 'btn btn-outline-primary btn-block mt-1'
-                        >Agregar</button>
-                    </form>
-
-
-
+                    <TodoAdd 
+                    handleAddTodo= { handleAddTodo }/>
                 </div>
             </div>
 
